f3ec11dec708f9dab2bd69a45cecd0a1409ef16c
lyle Sanders <lylesanders66@yahoo.com>
2016-08-23 16:46:04 -0400

    3 5 and 15 modulus

--- mined_minds_function.rb
+++ mined_minds_function.rb
@@ -4,9 +4,9 @@ def mined_minds(number) # define function mined_minds to pass a numbefr
 	 # number
 	if number % 15 == 0
 		"mined minds"
-		elsif number == 3
+		elsif number % 3 == 0
 			"mined"
-		elsif number == 5
+		elsif number % 5 == 0
 			"minds"
 		else 
 			number
