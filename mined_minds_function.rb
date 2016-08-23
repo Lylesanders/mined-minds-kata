97a71b4a6e5e4746c2de79bb4ea4d27517377343
lyle Sanders <lylesanders66@yahoo.com>
2016-08-23 16:22:41 -0400

    3=mined 5 = minds 15 =mined minds with modulus

--- mined_minds_function.rb
+++ mined_minds_function.rb
@@ -2,14 +2,14 @@
 
 def mined_minds(number) # define function mined_minds to pass a numbefr
 	 # number
-	if number == 15
+	if number % 15 == 0
 		"mined minds"
-	elsif number == 3
-		"mined"
-	elsif number == 5
-		"minds"
-	else 
-		number
+		elsif number == 3
+			"mined"
+		elsif number == 5
+			"minds"
+		else 
+			number
 	end
 
 	end
