41c1a8bfbd419556705c57bf3c1eecbe0e35310f
lyle Sanders <lylesanders66@yahoo.com>
2016-09-07 14:55:56 -0400

    add test to return nothing if no winner

--- test_lotto_function.rb
+++ test_lotto_function.rb
@@ -8,4 +8,10 @@ class TestLotteryFunction < Minitest::Test
 		my_number = "2567"
 		assert_equal(["2567"], good_luck?(winners, my_number))
 	end
+	def test_to_check_for_looser		#this is the test
+		winners = ["1234","2567", "1976","3274"]
+		my_number = "5567"
+		assert_equal([], good_luck?(winners, my_number))
+	end
+
 end
