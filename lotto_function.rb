2adb981bb6e786d5a080249a327afe6a45c7a2c5
lyle Sanders <lylesanders66@yahoo.com>
2016-09-07 20:46:54 -0400

    return for match 3

--- lotto_function.rb
+++ lotto_function.rb
@@ -1,7 +1,39 @@
 def good_luck?(winners, my_number)
 	my_winner=[] 	#sets up empty array 
-	if winners.include?(my_number)
-		my_winner << my_number
+	if winners.include?(my_number)	#compares for my_number in array winners
+		my_winner << my_number # moves my winner into output array
 	end
-	my_winner
+	my_winner # moves my_winner into good luck
 end
+def off_by_one(winners, my_number)
+	my_winner=[]	#set up the empty array
+	# nav_array = winners.length  #code to start moving through the array
+	nav_array = 3			# hard code to create 4 loops
+	pars_array = 0			# creates the pointer for the array element
+	pars_string=0			# sets the pointer for the character in the string
+	match = 0				# sets up a flag to move winners into my_winners
+	 while pars_array < nav_array do	# starts the pointer for the array
+	 	
+	 		
+	 		if winners[pars_array][0]== my_number[0]
+				match =match + 1
+			end
+			if winners[pars_array][1]== my_number[1]
+				match =match + 1
+			end
+			if winners[pars_array][2]== my_number[2]
+				match =match + 1
+			end
+			if winners[pars_array][3]== my_number[3]
+				match =match + 1
+			end
+
+	 		if match == 3           # moves winer[pars_array] into the my winners array
+	 			my_winner << winners[pars_array]
+	 		end
+	 		pars_array = pars_array +1
+	 		match = 0
+	 	end
+	 	
+	 my_winner   #moves my_winner into good_luck?
+	 end
--- test_lotto_function.rb
+++ test_lotto_function.rb
@@ -3,15 +3,19 @@ require_relative "lotto_function.rb"
 
 class TestLotteryFunction < Minitest::Test
 
-	def test_to_check_for_winner		#this is the test
+	def test_to_check_for_winner		#check for matching number in an array
 		winners = ["1234","2567", "1976","3274"]
 		my_number = "2567"
 		assert_equal(["2567"], good_luck?(winners, my_number))
 	end
-	def test_to_check_for_looser		#this is the test
+	def test_to_check_for_looser		
 		winners = ["1234","2567", "1976","3274"]
 		my_number = "5567"
 		assert_equal([], good_luck?(winners, my_number))
 	end
-
+	def test_to_check_for_match_three		
+		winners = ["1234","2567", "1976","3274"]
+		my_number = "5567"
+		assert_equal(["2567"], off_by_one(winners, my_number))
+	end
 end
